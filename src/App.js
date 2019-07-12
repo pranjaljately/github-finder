8c04fcc9afc1d14d5474b6348749ef4414a3a0f0
Pranjal <pjately078@gmail.com>
2019-07-12 12:17:43 +0100

    NotFound component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
 import User from './components/users/User';
 import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 
 import About from './components/pages/About';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -38,6 +39,7 @@ const App = () => {
                 <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
